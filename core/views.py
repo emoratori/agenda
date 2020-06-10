314388a4b78ca9974ffa92ce1a73d590defef17a
Erik Moratori <emoratori@yahoo.com.br>
2020-06-10 19:56:12 -0300

    Incluido função e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def eventos(request, titulo_evento):
     return HttpResponse('Local 2: {}'.format(titulo_evento))
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username1 = request.POST.get('username')
+        password1 = request.POST.get('password')
+        usuario = authenticate(username=username1, password=password1)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+    return redirect('/')
+
 #def index(request):
 #    return redirect('/agenda/')
-
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario_logado = request.user
-    #evento = Evento.objects.filter(usuario=usuario_logado)
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario_logado)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
