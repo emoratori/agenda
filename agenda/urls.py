58e3388227ea32ca93a3d7099c73f5e22c2a7361
Erik Moratori <emoratori@yahoo.com.br>
2020-06-10 22:48:17 -0300

    Incluido templates para inserção de eventos da agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,7 +20,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventos/<titulo_evento>', views.eventos),
+    #path('eventos/<titulo_evento>', views.eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit',views.submit_evento),
     path('agenda/', views.lista_eventos),
     path('',RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
--- core/views.py
+++ core/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
-def eventos(request, titulo_evento):
-    return HttpResponse('Local 2: {}'.format(titulo_evento))
+#def eventos(request, titulo_evento):
+#    return HttpResponse('Local 2: {}'.format(titulo_evento))
 
 def login_user(request):
     return render(request, 'login.html')
@@ -35,3 +35,19 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario_logado)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
